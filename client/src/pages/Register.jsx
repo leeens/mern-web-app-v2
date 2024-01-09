9ed20f35512f3da34a5784775c673ab3f97c4a7c
Elena Suslova <suslova@outlook.com>
2024-01-09 16:21:08 +0300

    first commit

---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,47 @@
+import { Link, Form, redirect } from 'react-router-dom'
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage'
+import { Logo, FormRow, SubmitBtn } from '../components'
+import customFetch from '../utils/customFetch'
+import { toast } from 'react-toastify'
+
+export const action = async ({ request }) => {
+  const formData = await request.formData()
+  const data = Object.fromEntries(formData)
+  try {
+    await customFetch.post('/auth/register', data)
+    toast.success('Registration successful')
+    return redirect('/login')
+  } catch (error) {
+    toast.error(error?.response?.data?.msg)
+    return error
+  }
+}
+
+function Register() {
+  return (
+    <Wrapper>
+      <Form method="post" className="form">
+        <Logo />
+        <h4>Register</h4>
+        <FormRow type="text" name="name" defaultValue="john" />
+        <FormRow
+          type="text"
+          name="lastName"
+          labelText="last name"
+          defaultValue="Smith"
+        />
+        <FormRow type="text" name="location" />
+        <FormRow type="email" name="email" />
+        <FormRow type="password" name="password" />
+        <SubmitBtn />
+        <p>
+          Already a member?
+          <Link to="/login" className="member-btn">
+            Login
+          </Link>
+        </p>
+      </Form>
+    </Wrapper>
+  )
+}
+export default Register
