2937ce505e22d0ff04a900dc661557db8702e596
Elena Suslova <suslova@outlook.com>
2024-01-09 16:54:30 +0300

    removing defaults

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -23,13 +23,8 @@ function Register() {
       <Form method="post" className="form">
         <Logo />
         <h4>Register</h4>
-        <FormRow type="text" name="name" defaultValue="john" />
-        <FormRow
-          type="text"
-          name="lastName"
-          labelText="last name"
-          defaultValue="Smith"
-        />
+        <FormRow type="text" name="name" />
+        <FormRow type="text" name="lastName" labelText="last name" />
         <FormRow type="text" name="location" />
         <FormRow type="email" name="email" />
         <FormRow type="password" name="password" />
