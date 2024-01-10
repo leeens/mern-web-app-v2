4563797e7ef3490645391482e60ab7785c17d677
Elena Suslova <suslova@outlook.com>
2024-01-10 16:21:19 +0300

    changes in uploading avatar

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -4,20 +4,23 @@ import { FormRow, Logo, SubmitBtn } from '../components'
 import customFetch from '../utils/customFetch'
 import { toast } from 'react-toastify'
 
-export const action = async ({ request }) => {
-  const formData = await request.formData()
-  const data = Object.fromEntries(formData)
-  try {
-    await customFetch.post('/auth/login', data)
-    toast.success('Login successful')
-    return redirect('/dashboard')
-  } catch (error) {
-    toast.error(error?.response?.data?.msg)
-    return error
+export const action =
+  (queryClient) =>
+  async ({ request }) => {
+    const formData = await request.formData()
+    const data = Object.fromEntries(formData)
+    try {
+      await customFetch.post('/auth/login', data)
+      queryClient.invalidateQueries()
+      toast.success('Login successful')
+      return redirect('/dashboard')
+    } catch (error) {
+      toast.error(error?.response?.data?.msg)
+      return error
+    }
   }
-}
 
-function Login() {
+const Login = () => {
   const navigate = useNavigate()
 
   const loginDemoUser = async () => {
@@ -37,15 +40,15 @@ function Login() {
     <Wrapper>
       <Form method="post" className="form">
         <Logo />
-        <h4>Login</h4>
+        <h4>login</h4>
         <FormRow type="email" name="email" />
         <FormRow type="password" name="password" />
         <SubmitBtn />
-        <button type="submit" className="btn btn-block" onClick={loginDemoUser}>
+        <button type="button" className="btn btn-block" onClick={loginDemoUser}>
           explore the app
         </button>
         <p>
-          Not a member yet ?
+          Not a member yet?
           <Link to="/register" className="member-btn">
             Register
           </Link>
--- middleware/validationMiddleware.js
+++ middleware/validationMiddleware.js
@@ -16,10 +16,13 @@ const withValidationErrors = (validateValues) => {
       const errors = validationResult(req)
       if (!errors.isEmpty()) {
         const errorMessages = errors.array().map((error) => error.msg)
+
+        const firstMessage = errorMessages[0]
+        console.log(Object.getPrototypeOf(firstMessage))
         if (errorMessages[0].startsWith('no job')) {
           throw new NotFoundError(errorMessages)
         }
-        if (errorMessages[0].startsWith('not authorized ')) {
+        if (errorMessages[0].startsWith('not authorized')) {
           throw new UnauthorizedError('not authorized to access this route')
         }
         throw new BadRequestError(errorMessages)
@@ -78,12 +81,12 @@ export const validateRegisterInput = withValidationErrors([
 ])
 
 export const validateLoginInput = withValidationErrors([
-  body('password').notEmpty().withMessage('password is required'),
   body('email')
     .notEmpty()
     .withMessage('email is required')
     .isEmail()
     .withMessage('invalid email format'),
+  body('password').notEmpty().withMessage('password is required'),
 ])
 
 export const validateUpdateUserInput = withValidationErrors([
